d058cc333c89002af76b8cf87078a1ccaf3ef588
Snapky06 <136767231+Snapky06@users.noreply.github.com>
2025-08-28 12:25:11 -0600

    Add files via upload

---
+++ progra2p2/src/progra2p2/Main.java
@@ -0,0 +1,75 @@
+package progra2p2;
+
+import java.io.IOException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+
+    static Scanner lea = new Scanner(System.in);
+    static MyFile mf = new MyFile();
+
+    public static void main(String[] args) {
+        int option = 0;
+
+        do {
+            System.out.println("\n--- MENU ---");
+            System.out.println("1- Set File");
+            System.out.println("2- Info");
+            System.out.println("3- Crear archivo");
+            System.out.println("4- Crear folder");
+            System.out.println("5- Borrar directorio");
+            System.out.println("6- Arbol");
+            System.out.println("7- Salir");
+            System.out.print("Elija una opcion: ");
+
+            try {
+                option = lea.nextInt();
+
+                switch (option) {
+                    case 1:
+                        set();
+                        break;
+                    case 2:
+                        mf.info();
+                        break;
+                    case 3:
+                        mf.crearArchivo();
+                        break;
+                    case 4:
+                        mf.crearFolder();
+                        break;
+                    case 5:
+                        mf.borrarDirectorio();
+                        break;
+                    case 6:
+                        mf.tree();
+                        break;
+                    case 7:
+                        System.out.println("Saliendo...");
+                        break;
+                    default:
+                        System.out.println("Opción inválida");
+                        break;
+                }
+
+            } catch (InputMismatchException e) {
+                lea.nextLine();
+                System.out.println("Por favor ingrese una opcion correcta");
+            } catch (NullPointerException e) {
+                System.out.println("Primero debe usar la opción 1 (Set File)");
+            } catch (IOException e) {
+                System.out.println("Otro tipo de error");
+            }
+
+        } while (option != 7);
+    }
+
+    public static void set() {
+        lea.nextLine();
+        System.out.print("Ingrese la ruta del archivo/carpeta: ");
+        String dir = lea.nextLine();
+        mf.setFile(dir);
+        System.out.println("Ruta asignada con exito!");
+    }
+}
